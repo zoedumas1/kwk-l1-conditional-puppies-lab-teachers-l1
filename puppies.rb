f1f6c3c970669da3d26a3435d5c14ec532234fbf
Zoe Dumas <zoe.dumas1@yahoo.com>
2018-06-19 20:37:13 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -7,7 +7,7 @@
 
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
-less puppies && "'Good job, Rachel!"
+less puppies && "Good job, Rachel!"
 
 
 # BONUS
